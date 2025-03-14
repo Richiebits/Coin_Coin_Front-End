d5c19df058d146d7f03c51992d95e4812709c869
nikolasunjka <nikolasunjka23@gmail.com>
2025-03-14 15:30:14 -0400

    ajout de animation page graphique

--- JavaScript/graphique.js
+++ JavaScript/graphique.js
@@ -6,6 +6,7 @@ function init() {
     let fileName = location.href;
     if (fileName.includes("graphiques.html")) {
         populateGraphique();
+        populateGraphiqueProjet();
     }
     if (fileName.includes("projets.html")) {
         populateGraphiqueProjet();
@@ -15,16 +16,41 @@ function init() {
 // Fonction pour changer le titre lorsqu'un projet est cliqué a partir de la page graphique.html
 function populateGraphique() {
     let projets = document.querySelectorAll(".projet");
-    let titrePlaceholder = document.querySelector("#contenuProjet .placeholder:first-child");
+    let titrePlaceholder = document.querySelector("#titre");
     
     projets.forEach(projet => {
         projet.addEventListener("click", function() {
             titrePlaceholder.textContent = this.textContent;
+            highlightSelectedProject();
         });
     });
 }
 
-// Fonction pour changer le titre lorsqu'un projet est cliqué a partir de la page graphique.html
-function populateGraphiqueProjet(){
 
+// Fonction pour changer le titre lorsqu'un projet est cliqué
+function populateGraphiqueProjet() {
+    let titrePlaceholder = document.querySelector("#titre");
+
+    let params = new URLSearchParams(window.location.search);
+    
+    let title = params.get("titre");
+
+    if (title) {
+        titrePlaceholder.textContent = title;
+    }    
+    highlightSelectedProject();
+}
+
+// Fonction pour mettre en surbrillance le projet correspondant au titre
+function highlightSelectedProject() {
+    let titrePlaceholder = document.querySelector("#titre").textContent;
+    let projets = document.querySelectorAll(".projet");
+    
+    projets.forEach(projet => {
+        if (projet.textContent === titrePlaceholder) {
+            projet.classList.add("highlight");
+        } else {
+            projet.classList.remove("highlight");
+        }
+    });
 }
