3ce234d16eabe4351ad15d5808c2434abd8e9546
TremblayElie <tremblay.elie@gmail.com>
2025-03-18 11:33:35 -0400

    début compte

---
+++ JavaScript/compte.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    init();
+})
+
+function init(){
+    const idCompte = sessionStorage.getItem("userId");
+
+    const bModifer = document.getElementById("bModifier")
+    const bConfirmer = document.getElementById("bConfimer");
+    const bAnnuler = document.getElementById("bAnnuler")
+    
+
+}
