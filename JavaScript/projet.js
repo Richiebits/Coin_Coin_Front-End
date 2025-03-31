0ce72f3c75410535c2879051fa8288eac3d9ab73
Justin Lavoie <justlav05@gmail.com>
2025-03-31 17:00:03 -0400

    Ajout projet par budget

--- JavaScript/projet.js
+++ JavaScript/projet.js
@@ -49,8 +49,7 @@ async function creerProjetDate() {
     const TBMontantCible = document.getElementById('TBMontantCible').value;
     const TBDateCible = document.getElementById('TBDateCible').value;
     const idClient = sessionStorage.getItem("id");
-    const dataProjet = { nom:TBNom, but_epargne:TBMontantCible, client_id:idClient };
-    const dataBudget = { client_id:idClient, depense_total:null, revenus_total:null, date_fin:TBDateCible};
+    const dataProjet = { nomProjet:TBNom, but_epargne:TBMontantCible, client_id:idClient, retraits_total:null, depots_total:null, date_fin:TBDateCible };
     
     //Vérifie nom du projet
     const projets = await fetchInfo("projet/client/" + idClient,
@@ -76,24 +75,34 @@ async function creerProjetDate() {
             "POST",
             {'Content-Type': 'application/json'},
             dataProjet);
-    
-        //Fait l'ajout du budget
-        fetchInfo("budget",
-            "POST",
-            {'Content-Type': 'application/json'},
-            dataBudget);
     } catch (error) {
         console.error("Création de projet et budget échoué : " + error);
     }
     //retourne vers page projet ou vers son graphique
     window.location.href = "graphiques.html?titre="+ encodeURIComponent(TBNom);
 }
+
 function creerProjetBudget() {
+    //Récupération des infos 
     const TBNom = document.getElementById('TBNom').value ;
     const TBMontantCible = document.getElementById('TBMontantCible').value;
     const TBDepot = document.getElementById('TBDepot').value ;
     const TBRetrait = document.getElementById('TBRetrait').value ; 
-    console.log("Budget coché");
+    const idClient = sessionStorage.getItem("id");
+
+    //Récupération des dépot et retrait
+    const TBFrequenceDepot = document.getElementById('frequenceDepot');
+    let freqDepot= TBFrequenceDepot.options[TBFrequenceDepot.selectedIndex].value;
+    freqDepot = getIndexFrequence(freqDepot);
+    const TBFrequenceRetrait = document.getElementById('frequenceRetrait');
+    let freqRetrait= TBFrequenceRetrait.options[TBFrequenceRetrait.selectedIndex].value;
+    freqRetrait = getIndexFrequence(freqRetrait);
+
+    //Préparation des données à envoyer dans les fetch
+    const dataProjet = { nomProjet:TBNom, but_epargne:TBMontantCible, client_id:idClient, 
+                         retraits_total:TBRetrait, depots_total:TBDepot, date_fin:null,
+                         nomDepot:null, montantDepot:TBDepot, depot_recurrence:freqDepot,
+                         nomRetrait:null, montantRetrait:TBRetrait, retrait_recurrence:freqRetrait};
 
     //Doit vérifier qu'il y a au moins un revenu d'entré
     if (!TBNom || !TBMontantCible || !TBDepot || !TBRetrait) {
@@ -101,8 +110,33 @@ function creerProjetBudget() {
         console.log("MANQUE INFO");
         return;
     }
+    
+    try {
+        //Fait l'ajout du projet
+        fetchInfo("projet",
+            "POST",
+            {'Content-Type': 'application/json'},
+            dataProjet);
+        
+        alert("Création de projet réussi ! ");
+    } catch (error) {
+        console.error("Création de projet et budget échoué : " + error);
+    }
+    //retourne vers son graphique
+    window.location.href = "graphiques.html?titre="+ encodeURIComponent(TBNom);
+}
 
-    //a ajouter la fonction qui calcule pour trouver le nb de jour que sa va prendre et la date cible
-    
-    
+function getIndexFrequence(x) {
+    let freq;
+    switch (x) {
+        case "hebdo" :   freq = 7;
+                         break;
+        case "bihebdo" : freq = 14;
+                         break;
+        case "mensuel" : freq = 30;
+                         break;
+        case "annuel" :  freq = 365;
+                         break;
+    }
+    return freq;
 }
